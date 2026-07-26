e523269d7626aa53679363e9617b731027f7ad7c
tshreya05 <tiwarishreyaa08@gmail.com>
2026-07-26 12:02:41 +0530

    Update frontend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "atsight-production.up.railway.app";
 
 export const shortlistCandidates = async (formData, onUploadProgress) => {
   const res = await axios.post(`${API_BASE}/api/v1/shortlist`, formData, {
